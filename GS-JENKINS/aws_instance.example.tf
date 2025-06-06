71e7b19bfab6c5d3477e795cc930a549b98395d6
Sharath3116 <sharath3192@gmail.com>
2025-06-06 12:02:46 +0530

    testing scripts

---
+++ GS-JENKINS/aws_instance.example.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "example" {
+  # No need to define everything before import,
+  # but after import, you must match config to actual state
+}
+
+resource "aws_instance" "my_instance" {
+  # placeholder for import
+}
